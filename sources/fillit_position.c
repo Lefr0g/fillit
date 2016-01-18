a4696fa342dde42d6cfd51f72c8a48ae6de51280
lpoujade <lpoujade@student.42.fr>
2016-01-18 18:52:03 +0100

    added restriction of test : behind the axe

--- sources/fillit_position.c
+++ sources/fillit_position.c
@@ -6,7 +6,7 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 15:15:19 by amulin            #+#    #+#             */
-/*   Updated: 2016/01/18 18:02:10 by amulin           ###   ########.fr       */
+/*   Updated: 2016/01/18 18:50:59 by lpoujade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -232,7 +232,7 @@ void	fillit_move_and_try(t_env *e, t_tetri *moving, int x, int y)
 
 	siz_square = 0;
 	j = 0;
-	while (j < 4 && (!e->smallest_size || fillit_square_size(e) < e->smallest_size))
+	while (j < 4 && (!e->smallest_size || fillit_square_size(e) < e->smallest_size) && x >=0 && y >= 0)
 	{
 		moving->x_offset = x - moving->x[j];
 		moving->y_offset = y - moving->y[j];
@@ -257,14 +257,12 @@ void	fillit_move_and_try(t_env *e, t_tetri *moving, int x, int y)
 					e->result = fillit_save_printable(e);
 					ft_putendl(e->result);
 				}
-				else
-					ft_putendl(fillit_save_printable(e));
+//				else
+//					ft_putendl(fillit_save_printable(e));
 //				ft_putchar('|');
 			}
 			else
-			{
 				fillit_move_around(e);
-			}
 			moving->fixed = 0;
 			moving->firstmove = 1;
 //			debug_inception_print(e);
