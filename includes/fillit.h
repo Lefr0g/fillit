72d5ce67d73a4131b5d64e61c3efb92e88c827f3
Antoine MULIN <amulin@e3r2p9.42.fr>
2016-01-26 14:47:50 +0100

    Created fillit_solve.c, file re-organization in progress

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/28 16:49:38 by amulin            #+#    #+#             */
-/*   Updated: 2016/01/26 12:46:34 by amulin           ###   ########.fr       */
+/*   Updated: 2016/01/26 14:46:08 by amulin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,9 +116,7 @@ int		fillit_new_tetri(t_list **list_ptr, t_tetri **tetri_ptr);
 /*
 ** fillit_calc.c
 */
-int		fillit_calc(t_env *start);
-int		fillit_square_size(t_env *e);
-void	fillit_square_get_range(t_vars *v);
+//int		fillit_calc(t_env *start);
 void	fillit_load_xy(t_env *e);
 void	fillit_xy_get(t_tetri *t_ptr);
 void	fillit_xy_correct(t_tetri *ptr);
@@ -138,11 +136,18 @@ void	fillit_print_colored(char *map);
 ** fillit_position.c
 */
 int		fillit_check_collision(t_env *e, t_tetri *moving);
+int		fillit_square_size(t_env *e);
+void	fillit_square_get_range(t_vars *v);
 int		fillit_xy_collision(int x, int y, t_tetri *ptr);
+void	fillit_liveprint(t_env *e);
+void	debug_inception_print(t_env *e);
+
+/*
+** fillit_solve.c
+*/
 void	fillit_solve(t_env *e);
 void	fillit_move_around(t_env *e, t_vars *t);
 void	fillit_move_and_try(t_env *e, t_tetri *moving, int x, int y);
-void	fillit_liveprint(t_env *e);
-void	debug_inception_print(t_env *e);
+void	fillit_check_solution(t_env *e, int *siz_square);
 
 #endif
