1eb0fbf58ba1a651d4b61ca1d5af95cf244562bb
Lefr0g <amulin@student.42.fr>
2016-01-27 16:47:17 +0100

    Added ft_stdin_check.c, to be added to libft

---
+++ sources/ft_stdin_check.c
@@ -0,0 +1,43 @@
+
+
+#include "libft.h"
+#include <fcntl.h>
+
+/*
+** This function is designed as a fuse prior to a gnl() call in a parsing
+** situation.
+** It returns the number of occurences of the 'trigger' char within the
+** first 'lenght' bytes of the file.
+** This way we can then avoid using gnl on /dev/zero or /dev/random
+** for example.
+*/
+
+int	ft_stdin_check(char *filename, char trigger, int lenght)
+{
+	int		i;
+	int		fd;
+	int		readret;
+	int		ret;
+	char	*buf;
+
+	if (!(buf = ft_strnew(lenght)))
+		return (-1);
+	if ((fd = open(filename, O_RDONLY)) < 0)
+		return (-2);
+	if ((readret = read(fd, buf, lenght)) == -1)
+		ret = -3;
+	else
+	{
+		ret = 0;
+		i = 0;
+		while (buf[i])
+		{
+			if (buf[i] == trigger)
+				ret++;
+			i++;
+		}
+	}
+	if (close(fd) == -1)
+		return (-4);
+	return (ret);
+}
