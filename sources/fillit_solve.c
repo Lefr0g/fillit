2ae84c1ac12f9427218c780a86603dc490dd9fcf
Antoine MULIN <amulin@e3r3p6.42.fr>
2016-02-10 16:24:25 +0100

    Norme fillit_solve.c:w

--- sources/fillit_solve.c
+++ sources/fillit_solve.c
@@ -6,37 +6,14 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/26 14:40:53 by amulin            #+#    #+#             */
-/*   Updated: 2016/02/10 15:50:23 by amulin           ###   ########.fr       */
+/*   Updated: 2016/02/10 16:23:31 by amulin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-/*
-** Inversion de la logique de resolution :
-** 0/ Choisir une taille de carre (en partant du plus grand cote parmi tous
-**    les tetris)
-** 1/ Essayer toutes les combinaisions recursivement
-**    1.0/ Si tous les tetris rentrent : sauver le resultat
-**    1.1/ Sinon, augmenter le carre de 1 et retour en 1/
-*/
-
-int 	fillit_solve(t_env *e, t_list *moving)
+int		fillit_browse_square(t_env *e, t_list *moving, t_tetri *tet_ptr)
 {
-	t_list	*lst_ptr;
-	t_tetri	*tet_ptr;
-
-	if (!moving && e->tcount == e->tlocked)
-	{
-		e->result = fillit_get_output_map(e);
-		return (1);
-	}
-	lst_ptr = moving;
-	tet_ptr = (t_tetri*)moving->content;
-
-	tet_ptr->x_offset = 0;
-	tet_ptr->y_offset = 0;
-	
 	while (tet_ptr->y_offset + tet_ptr->y_max < e->square_size)
 	{
 		while (tet_ptr->x_offset + tet_ptr->x_max < e->square_size)
@@ -55,7 +32,26 @@ int 	fillit_solve(t_env *e, t_list *moving)
 		tet_ptr->x_offset = 0;
 		tet_ptr->y_offset++;
 	}
+	return (0);
+}
+
+/*
+** This recursive function uses backtracking to find the best tetri assembly
+*/
+
+int		fillit_solve(t_env *e, t_list *moving)
+{
+	t_tetri	*tet_ptr;
+
+	if (!moving && e->tcount == e->tlocked)
+	{
+		e->result = fillit_get_output_map(e);
+		return (1);
+	}
+	tet_ptr = (t_tetri*)moving->content;
 	tet_ptr->x_offset = 0;
 	tet_ptr->y_offset = 0;
+	if (fillit_browse_square(e, moving, tet_ptr))
+		return (1);
 	return (0);
 }
